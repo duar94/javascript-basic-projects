0f89de8425c0724ca83dc43fc18f083e6e034012
DuarHydro <christian.duarte@hydrotecna.com>
2024-10-07 12:37:08 +0200

    color flipper done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "orange", "purple"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+    const newColor = colors[randomNumber];
+  document.body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+  color.style.color = newColor;// Cambia el color del texto del span
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
